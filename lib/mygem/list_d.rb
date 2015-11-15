fc2c83c207bca9a77d1575a2aeb14d5458ed16bf
Eneas Melian <alu0100785753@ull.edu.es>
2015-11-15 22:38:00 +0000

    Creada una nueva clase para almacenar la lista doblemente enlazada a partir de la lista enlazada simple

---
+++ lib/mygem/list_d.rb
@@ -0,0 +1,55 @@
+class List_d
+
+  attr_accessor :ini
+  def initialize
+    @ini=Node.new(nil,nil)
+  end
+
+  def push(elemento)
+    if @ini.value == nil
+      @ini.value= elemento
+    else
+      aux=@ini
+      while aux.next != nil do
+        aux=aux.next
+      end
+      aux.next= Node.new(elemento,nil)
+    end
+  end
+
+  def push_var(*elementos)
+    if @ini.value == nil
+      @ini.value= elementos[0]
+      elementos.shift
+      aux=@ini
+      for x in elementos
+        aux.next= Node.new(x,nil)
+        aux=aux.next
+      end
+    else
+      aux=@ini
+      while aux.next != nil do
+        aux=aux.next
+      end
+      for x in elementos
+        aux.next= Node.new(x,nil)
+        aux=aux.next
+      end
+    end
+  end
+
+  def pop
+    aux=@ini
+    if aux.next==nil
+      @ini=Node.new(nil,nil)
+    else
+      @ini=aux.next
+    end
+    return aux.value
+  end
+  
+  def head
+    return @ini.value
+  end
+
+end
